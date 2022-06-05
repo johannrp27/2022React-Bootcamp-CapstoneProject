fa8f25ee21ffa139a00b0d334cefaa5a99835e91
Johann Rojas <johannrip@gmail.com>
2022-06-05 10:55:01 -0500

    chore: add config font awesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './styles/theme.scss';
+import './react-fontawesome-config';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/react-fontawesome-config.js
@@ -0,0 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+import { } from "@fortawesome/free-regular-svg-icons";
+
+
+library.add(
+  faCartShopping
+)
