e64561acef568495f215f7801973ea63c8c08881
Johann Rojas <johannrip@gmail.com>
2022-06-05 19:24:46 -0500

    chore: add search icon

--- src/react-fontawesome-config.js
+++ src/react-fontawesome-config.js
@@ -1,10 +1,11 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 import { } from "@fortawesome/free-regular-svg-icons";
 
 
 library.add(
-  faCartShopping
+  faCartShopping,
+  faMagnifyingGlass
 )
