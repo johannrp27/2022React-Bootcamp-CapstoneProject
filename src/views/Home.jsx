ddf15f1294d6185c28e12bec71b13a7340ce5a65
Johann Rojas <johannrip@gmail.com>
2022-06-08 23:49:30 -0500

    refactor: remove useless view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import HomePage from './HomePage';
-import Footer from '../components/Footer';
+
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import GridCategories from './GridCategories'
+import GridFeatured from './GridFeatured'
+import BannerSlider from '../components/BannerSlider'
+
+import categoriesData from '../assets/mocks/en-us/product-categories.json'
+import featuredData from '../assets/mocks/en-us/featured-products.json'
+import bannersData from '../assets/mocks/en-us/featured-banners.json'
+
+import styles from '../styles/Home.module.scss'
 
 const Home = () => {
   return (
     <>
       <Header />
-      <HomePage />
+
+      <div className="">
+        <div className={`d-flex flex-column p-3 p-md-4 gap-5 ${styles.homepage}`}>
+          <BannerSlider data={bannersData} />
+          <GridCategories data={categoriesData} />
+          <GridFeatured data={featuredData} />
+        </div>
+      </div>
+
       <Footer />
     </>
   );
