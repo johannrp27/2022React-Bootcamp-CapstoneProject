1f44774db4cf4af1ab34107d65ee0127db4700b9
Johann Felipe Rojas <johannrip@gmail.com>
2022-06-13 17:51:46 -0500

    feat: create custom hook to handle filters

---
+++ src/utils/hooks/useFilterProducts.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
+import mockResponse from '../../assets/mocks/en-us/products.json'
+
+export const useFilterProducts = () => {
+  const products = mockResponse.results;
+  const initialFilters = {
+    'bed--bath' : false,
+    'lighting' : false,
+    'kitchen': false,
+    'furniture': false,
+    'decorate--organize': false,
+  }
+
+  const [filters, setFilters] = useState(initialFilters)
+  const [filteredProducts, setFilteredProducts] = useState(products)
+  const [isLoading, setIsLoading] = useState(true);
+
+  const isAnyActiveFilter = () => {
+    return Object.values(filters).some(filter => filter);
+  }
+  const filterProducts = () => {
+    if(!isAnyActiveFilter()) {
+      return products
+    } else {
+      return products.filter(({data}) => {
+        return filters[data.category.slug];
+      })
+    }
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      const prods = filterProducts()
+      setFilteredProducts(prods);
+      setIsLoading(false);
+    }, 2000)
+  }, [filters])
+
+  return {
+    filters,
+    filteredProducts,
+    setFilters,
+    isLoading,
+  }
+
+}
