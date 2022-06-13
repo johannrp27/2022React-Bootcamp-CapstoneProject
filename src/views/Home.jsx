cff24cc0f6ea7e1d7c304bd4c8fd1ddbb9142129
Johann Felipe Rojas <johannrip@gmail.com>
2022-06-13 17:51:30 -0500

    chore: toggle view on button and logo

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
 import GridCategories from './GridCategories'
-import GridFeatured from './GridFeatured'
+import ProductList from './ProductList';
+import GridProducts from './GridProducts';
 import BannerSlider from '../components/BannerSlider'
-
-import categoriesData from '../assets/mocks/en-us/product-categories.json'
 import featuredData from '../assets/mocks/en-us/featured-products.json'
-import bannersData from '../assets/mocks/en-us/featured-banners.json'
 
 import styles from '../styles/Home.module.scss'
 
 const Home = () => {
+  const [currentView, setCurrentView] = useState('Home')
+
   return (
     <>
-      <Header />
-
-      <div className="">
-        <div className={`d-flex flex-column p-3 p-md-4 gap-5 ${styles.homepage}`}>
-          <BannerSlider data={bannersData} />
-          <GridCategories data={categoriesData} />
-          <GridFeatured data={featuredData} />
-        </div>
+      <Header setCurrentView={setCurrentView} />
+      <div className={styles.home}>
+        {
+          currentView === 'Home' && (
+            <div className={`d-flex flex-column p-3 p-md-4 gap-5 ${styles.homepage}`}>
+              <BannerSlider />
+              <GridCategories />
+              <div>
+                <h4 className='mb-4'>Discover the high-tier products</h4>
+                <GridProducts
+                  products={featuredData.results} />
+                <button
+                  className="btn mt-4"
+                  onClick={() => setCurrentView('ProductList')}>View all products</button>
+              </div>
+            </div>
+          )
+        }
+        {
+          currentView === 'ProductList' && (
+            <ProductList />
+          )
+        }
       </div>
-
       <Footer />
     </>
   );
