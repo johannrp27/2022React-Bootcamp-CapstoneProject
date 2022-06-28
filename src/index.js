39e997e60dbb15c74aa0b7b9e3d779313346719a
Johann Felipe Rojas <johannrip@gmail.com>
2022-06-28 11:44:10 -0500

    chore: add router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/theme.scss';
 import './react-fontawesome-config';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
