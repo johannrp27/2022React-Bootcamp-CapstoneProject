b30c52e6f54e2bf0047f3b373c591ba4d02bb220
Johann Felipe Rojas <johannrip@gmail.com>
2022-07-01 21:08:25 -0500

    chore: add products to cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types';
 import styles from '../styles/ProductCard.module.scss';
 import { Link } from 'react-router-dom';
+import appContext from '../context/context';
 
-const ProductCard = ({id, name, sku, urlImage, alt, shortDescription, stock, price}) => {
+const ProductCard = ({id, name, urlImage, alt, shortDescription, stock, price}) => {
   const descriptionTrimmed = shortDescription?.slice(0, 80) + '...';
   const productName = name.split(' ').splice(0, 3).join(' ');
 
-  const handleProductClick = () => {
-    console.log('Added', sku);
+  const {addProductToCart} = useContext(appContext);
+  const toggleAddProduct = () => {
+    addProductToCart(id)
   }
   return (
-    <Link to={`/products/${id}`}>
-      <div className={styles.card}>
-        <ul className={styles.ul}>
-          <li>
-            <p className='m-0 fw-bold'>Stock {stock}</p>
-          </li>
-          <li>
-            <p className='m-0 fw-bold'>${price}</p>
-          </li>
-        </ul>
-        <img
-          className={styles.imgCard}
-          src={urlImage}
-          loading="lazy"
-          alt={alt} />
-        <div className={styles.conText}>
-          <h4 className='mb-3'>{productName}</h4>
-          <p className={styles.shortDesc}>
-            {descriptionTrimmed}
-            <button onClick={handleProductClick}>Add to cart</button>
-          </p>
+    <div className={styles.containerCard}>
+      <Link to={`/products/${id}`}>
+        <div className={styles.card}>
+          <ul className={styles.ul}>
+            <li>
+              <p className='m-0 fw-bold'>Stock {stock}</p>
+            </li>
+            <li>
+              <p className='m-0 fw-bold'>${price}</p>
+            </li>
+          </ul>
+          <img
+            className={styles.imgCard}
+            src={urlImage}
+            loading="lazy"
+            alt={alt} />
+          <div className={styles.conText}>
+            <h4 className='mb-3'>{productName}</h4>
+            <p className={styles.shortDesc}>
+              {descriptionTrimmed}
+            </p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+      <button
+        className={styles.btnAdd}
+        onClick={toggleAddProduct}>Add to cart</button>
+    </div>
   )
 }
 
@@ -42,7 +48,6 @@ ProductCard.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string,
   urlImage: PropTypes.string,
-  sku: PropTypes.string,
   alt: PropTypes.string,
   shortDescription: PropTypes.string,
   stock: PropTypes.number,
