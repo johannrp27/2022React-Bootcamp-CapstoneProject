ef954425f93d80f20d099fac3fe256b8c8cd290c
Johann Felipe Rojas <johannrip@gmail.com>
2022-07-02 23:06:30 -0500

    chore: add icon delete

--- src/react-fontawesome-config.js
+++ src/react-fontawesome-config.js
@@ -1,11 +1,12 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-import { faCartShopping, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faMagnifyingGlass, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 import { } from "@fortawesome/free-regular-svg-icons";
 
 
 library.add(
   faCartShopping,
-  faMagnifyingGlass
+  faMagnifyingGlass,
+  faTrashCan
 )
